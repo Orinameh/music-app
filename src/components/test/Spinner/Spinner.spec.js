c7a4bd2c805f069bdab2c7e9d87e785c869823f7
Orinameh <memefacto@yahoo.com>
2019-03-29 14:50:34 +0100

    added test for spinner

---
+++ src/components/test/Spinner/Spinner.spec.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Spinner from '../../layout/Spinner';
+
+describe('Spinner', () => {
+	let wrapper;
+	beforeEach(() => {
+		wrapper = shallow(<Spinner />)
+	})
+
+	it('should render correctly', () => {
+		expect(wrapper).toMatchSnapshot();
+	});
+
+	it('should render a <div />', () => {
+		expect(wrapper.find('div').length).toEqual(1)
+	})
+
+	it('should render a span', () => {
+		expect(wrapper.find('img').length).toEqual(1)
+	})
+})
